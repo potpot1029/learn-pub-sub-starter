3dca08f8c5431545999983ea3aeffc2cc4cf5245
potpot <chaukinyi1029@gmail.com>
2026-06-15 23:22:36 +0800

    done ch 2: message brokers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	const connUrl = "amqp://guest:guest@localhost:5672/"
+
 	fmt.Println("Starting Peril server...")
+
+	conn, err := amqp.Dial(connUrl)
+	if err != nil {
+		log.Fatalf("error starting Peril server: %v", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to Peril server!")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Shutting down Peril server...")
 }
